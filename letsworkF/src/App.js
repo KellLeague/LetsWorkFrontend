959aa11ba9fdbad6330abc0af7164a6fcffbe97b
Ivy Strickland <IvyStrickland@pursuit.org>
2019-04-10 14:41:28 -0400

    home video

--- letsworkF/src/App.js
+++ letsworkF/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { HashRouter, Route} from 'react-router-dom';
 import Header from './components/header';
-// import Home from './components/home';
+import Home from './components/home';
 import Login from './containers/login';
 import SignUp from './containers/signup';
 // import UserProfile from './components/userprofile';
@@ -44,7 +44,7 @@ class App extends Component {
         <>
       <Header/>
         <Route path='/signup' exact component={SignUp}/> 
-        {/* <Route path='/' exact component={Home}/> */}
+        <Route path='/' exact component={Home}/>
         <Route path='/login' exact component={Login}/>
         {/* <Route path='/containers/logout' exact component={Logout}/> */}
         {/* <Route path='/userprofile' exact component={UserProfile}/> */}
--- letsworkF/src/components/home.js
+++ letsworkF/src/components/home.js
@@ -8,7 +8,9 @@ class Home extends Component {
     return (
       <HashRouter>
         <>
+     
       <VideoHero/>
+      
       </>
       </HashRouter>
       
